a483a1b1892a68cbff6bf7b5cf927cba5a0704f3
[Edwin Velasco] <evelasco1423@gmail.com>
2023-03-20 11:18:56 -0400

    Initial commit

---
+++ src/main/java/Weekend.java
@@ -0,0 +1,97 @@
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.Arrays;
+
+public class Weekend {
+        public static void main(String[] args) {
+                // question 1
+        /*
+        String firstName = "Edwin";
+        String lastName = "Velasco";
+        String fullName = firstName + " " + lastName;
+        System.out.println(fullName);*/
+
+                //Question 2
+/*
+        int[] firstArray = {103,23,34,41,69};
+        int[] secondArray = {66,75,18,91,120};
+        // Multiply the element at index position 3 -
+        // in the first array by the element at index position 1 in the second array.
+        System.out.println((firstArray[3]*secondArray[1])/firstArray[1]);
+
+ */
+                //Question 3
+        /*
+        String [] Artists = {"Pluma","ToryLanes","ASAP"};
+        for (String A : Artists){
+            System.out.println(A);
+
+     //Question 4
+     public class Weekend {
+      static String a = "Learning Java Is Fun";
+
+        public static void main(String[] args) {
+        System.out.println(a);
+    }
+
+         */
+
+                //Question 5
+        /*
+        static void numbers(String num){
+                System.out.println(num);
+        }
+        public static void main(String[] args) {
+                numbers("0");
+                numbers("1");
+                numbers("2");
+                numbers("3");
+                numbers("4");
+                numbers("5");   // }
+
+                */
+
+                //Question 6
+          /*
+            for (int i = 13; i <= 19; i++) {
+                    System.out.println(i);
+            }
+
+           */
+
+                //Question 7
+            /*
+            for (int i = 13; i <= 19; i = i + 2) {
+                    System.out.println(i);
+            }
+
+             */
+
+                //Question 8
+                /*
+                int grade = 45;
+                if (grade >= 100) {
+                        System.out.println("You got a A ");
+                }else if (grade <= 89 && grade >= 80) {
+                        System.out.println("You got a B");
+                }else if (grade <= 79 && grade >= 70) {
+                        System.out.println("You got a C");
+                }else if (grade <= 69 && grade >= 60) {
+                        System.out.println("You got a D");
+                }else if (grade <= 59 && grade >= 0) {
+                        System.out.println("You got a F");
+                } */
+
+                //Question 9
+                int[] numbersArray = {2, 34, 7, 48, 0, 13, 24};
+                int sum = arr ;
+                System.out.println(sum);
+                for (int A : numbersArray) {
+                        System.out.println(A);
+                }
+        }
+
+
+}
+
+
